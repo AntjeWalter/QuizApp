139434f93b8e0c0ae959cb2410b142bdbd63d586
Antje Walter <115352305+AntjeWalter@users.noreply.github.com>
2022-11-04 10:30:47 +0100

    Feature/profile page (#6)
    * Add profile page basics
    
    * Add quote on profile
    
    * Add dark mode button
    
    * Change data-js class bodyDarkMode

--- js/index.js
+++ js/index.js
@@ -2,7 +2,13 @@ const answer = document.querySelector('[data-js="quizcard-answer"]');
 const answerButton = document.querySelector(
   '[data-js="quizcard-answer-button"]'
 );
+const button = document.querySelector('[data-js="buttonDarkMode"');
+const darkmode = document.querySelector('[data-js="bodyDarkMode"]');
 
 answerButton.addEventListener("click", () => {
   answer.classList.toggle("revealAnswer");
+});
+
+button.addEventListener("click", () => {
+  darkmode.classList.toggle("dark-mode");
 });

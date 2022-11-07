93fc44636fc95d54507f8a339ba17da2598e933a
Antje Walter <115352305+AntjeWalter@users.noreply.github.com>
2022-11-07 17:38:01 +0100

    Feature/bookmarktoggle (#7)
    * First attempt to toggle bookmark
    
    * Change bookmarks to images
    
    * Add toggle function to first bookmark

--- js/index.js
+++ js/index.js
@@ -2,8 +2,14 @@ const answer = document.querySelector('[data-js="quizcard-answer"]');
 const answerButton = document.querySelector(
   '[data-js="quizcard-answer-button"]'
 );
-const button = document.querySelector('[data-js="buttonDarkMode"');
+const quizcard = document.querySelector('[data-js="quizcard"]');
+const button = document.querySelector('[data-js="buttonDarkMode"]');
 const darkmode = document.querySelector('[data-js="bodyDarkMode"]');
+const bookmark = quizcard.querySelector('[data-js="bookmark-button"]');
+const bookmarkicon = quizcard.querySelector('[data-js="bookmark-icon"]');
+const bookmarkiconFilled = quizcard.querySelector(
+  '[data-js="bookmark-icon-filled"]'
+);
 
 answerButton.addEventListener("click", () => {
   answer.classList.toggle("revealAnswer");
@@ -11,4 +17,9 @@ answerButton.addEventListener("click", () => {
 
 button.addEventListener("click", () => {
   darkmode.classList.toggle("dark-mode");
+});
+
+bookmark.addEventListener("click", () => {
+  bookmarkicon.classList.toggle("hidden");
+  bookmarkiconFilled.classList.toggle("hidden");
 });
